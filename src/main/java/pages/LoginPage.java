26ba1e53d97a007efca389b88d02e8bf59e54754
Vita <cohen0402@gmail.com>
2025-11-03 19:47:57 +0200

    Homework 8 PropertiesReader

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -65,7 +65,9 @@ public class LoginPage extends BasePage {
         btnNotRegistered.click();
     }
 
-
+    baseUrl=https://ilcarro.web.app/search
+    login=iv@mail.com
+            password=123456Aa!
 
     public void typeLoginForm(User user) {
         inputEmail.sendKeys(user.getUsername());
