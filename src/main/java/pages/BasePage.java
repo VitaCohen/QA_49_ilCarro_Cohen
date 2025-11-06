6d6e04043e73ee99aee9fa2b3d5357a883b0017d
Vita <cohen0402@gmail.com>
2025-11-06 19:16:19 +0200

    added Search Negative tests, ClickWait

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -65,5 +66,30 @@ public abstract class BasePage {
         }
 
     }
+
+    static  void pause(int time){
+        try {
+            Thread.sleep(time*1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void clickWait(WebElement element, int time){
+        new  WebDriverWait(driver, Duration.ofSeconds(time))
+                .until(ExpectedConditions.elementToBeClickable(element))
+                .click();
+    }
+
+    public boolean urlContains(String fraction, int time) {
+        try {
+            return new WebDriverWait(driver, Duration.ofSeconds(time))
+                    .until(ExpectedConditions.urlContains(fraction));
+        }catch (TimeoutException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
 }
--- src/test/java/ui_tests/SearchCarTests.java
+++ src/test/java/ui_tests/SearchCarTests.java
@@ -1,6 +1,7 @@
 package ui_tests;
 
 import manager.ApplicationManager;
+import org.openqa.selenium.TimeoutException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -28,9 +29,62 @@ public class SearchCarTests extends ApplicationManager {
 
 //Homework 9
         // Assert.assertTrue(homePage.isFieldCityEmpty());
-        Assert.assertTrue(homePage.validateUrl());
+        //  Assert.assertTrue(homePage.validateUrl());
+        Assert.assertTrue(homePage.urlContains("results", 5));
     }
 
+    // будет падать, надо  изменить typeSearchFormWOJS убрать комент clickWait
+    @Test(expectedExceptions = org.openqa.selenium.TimeoutException.class)
+    public void searchNegativeTestWOCity() {
+        String city = "";
+        LocalDate dateFrom = LocalDate.of(2025, 12, 1);
+        LocalDate dateTo = LocalDate.of(2025, 12, 22);
+        homePage.typeSearchFormWOJS(city, dateFrom, dateTo);
+
+    }
+
+    @Test
+    public void searchNegativeTestWOCityValidateErrorMessage() {
+        String city = "";
+        LocalDate dateFrom = LocalDate.of(2025, 12, 1);
+        LocalDate dateTo = LocalDate.of(2025, 12, 22);
+        homePage.typeSearchFormWOJS(city, dateFrom, dateTo);
+        Assert.assertTrue(homePage.isTextInErrorPresent("City is required"));
+
+    }
+
+    @Test
+    public void searchNegativeTest_beforeToday() {
+        String city = "Haifa";
+        LocalDate dateFrom = LocalDate.of(2024, 12, 1);
+        LocalDate dateTo = LocalDate.of(2025, 12, 22);
+        homePage.typeSearchFormWOJS(city, dateFrom, dateTo);
+        Assert.assertTrue(homePage.isTextInErrorPresent("You can't pick date before today"));
+
+    }
+
+    @Test
+    public void searchNegativeTest_afterOneYear() {
+        String city = "Haifa";
+        LocalDate dateFrom = LocalDate.of(2024, 12, 1);
+        LocalDate dateTo = LocalDate.of(2027, 12, 22);
+        homePage.typeSearchFormWOJS(city, dateFrom, dateTo);
+        Assert.assertTrue(homePage.isTextInErrorPresent("You can't pick date before today"));
+
+    }
+
+    @Test
+    public void searchNegativeTest_firstDateAfterSecondDate() {
+        String city = "Haifa";
+        LocalDate dateTo = LocalDate.of(2025, 12, 1);
+        LocalDate dateFrom = LocalDate.of(2025, 12, 22);
+        homePage.typeSearchFormWOJS(city, dateFrom, dateTo);
+        Assert.assertTrue(homePage.isTextInErrorPresent("Second date must be after first date"));
+
+    }
+
+
+    //HW 9------------------------------
     @Test // BUG
     public void searchNegativeTest_emptyCity() {
         String city = "";
@@ -42,7 +96,7 @@ public class SearchCarTests extends ApplicationManager {
     }
 
 
-    @Test // BUG
+    @Test
     public void searchNegativeTest_wrongCity() {
         String city = "1111111";
         LocalDate dateFrom = LocalDate.of(2025, 12, 1);
@@ -71,5 +125,7 @@ public class SearchCarTests extends ApplicationManager {
         homePage.typeSearchForm(city, dateFrom, dateTo);
         Assert.assertTrue(homePage.validateUrl());
     }
+//------------------------
+
 
 }
